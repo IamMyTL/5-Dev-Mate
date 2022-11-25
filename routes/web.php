07d2d5c0de601dc621cf9b89082244e34c95af1e
JNagaaa <jordan.grauwmans@ifosup.wavre.be>
2022-11-25 13:43:38 +0100

    Patch bug redirection au clic sur nom de l'app

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/', function () {
+    return view('home');
+});
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']);
 
