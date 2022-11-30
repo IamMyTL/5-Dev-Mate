8478f553ce3405b9b21faa7fd670b92c354b93e9
MyTL <tlambert385@gmail.com>
2022-11-30 10:56:54 +0100

    Ajout des middleware auth

--- routes/web.php
+++ routes/web.php
@@ -26,21 +26,21 @@ Route::get('/auth/passwords/edit', [App\Http\Controllers\PasswordController::cla
 Route::post('/auth/passwords/update', [App\Http\Controllers\PasswordController::class, 'update']);
 
 
-Route::get('/market', [App\Http\Controllers\MarketController::class, 'index']);
+Route::get('/market', [App\Http\Controllers\MarketController::class, 'index'])->middleware('auth');
 
 Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->middleware('admin');
 
-Route::get('/ads/list/{id}', [App\Http\Controllers\AdsController::class, 'index']);
-Route::get('/ads/one/{id}', [App\Http\Controllers\AdsController::class, 'show']);
-Route::get('ads/create', [App\Http\Controllers\AdsController::class, 'create']);
-Route::post('/ads/store', [App\Http\Controllers\AdsController::class, 'store']);
-Route::put('/ads/update/{id}', [App\Http\Controllers\AdsController::class, 'update']);
-Route::get('/ads/edit/{id}', [App\Http\Controllers\AdsController::class, 'edit']);
-Route::get('/ads/delete/{id}', [App\Http\Controllers\AdsController::class, 'delete']);
+Route::get('/ads/list/{id}', [App\Http\Controllers\AdsController::class, 'index'])->middleware('auth');
+Route::get('/ads/one/{id}', [App\Http\Controllers\AdsController::class, 'show'])->middleware('auth');
+Route::get('ads/create', [App\Http\Controllers\AdsController::class, 'create'])->middleware('auth');
+Route::post('/ads/store', [App\Http\Controllers\AdsController::class, 'store'])->middleware('auth');
+Route::put('/ads/update/{id}', [App\Http\Controllers\AdsController::class, 'update'])->middleware('auth');
+Route::get('/ads/edit/{id}', [App\Http\Controllers\AdsController::class, 'edit'])->middleware('auth');
+Route::get('/ads/delete/{id}', [App\Http\Controllers\AdsController::class, 'delete'])->middleware('auth');
 
 
-Route::get('/profiles/one/{id}', [App\Http\Controllers\ProfileController::class, 'show']);
-Route::get('/profiles/edit/{id}', [App\Http\Controllers\ProfileController::class, 'edit']);
-Route::put('/profiles/update/{id}', [App\Http\Controllers\ProfileController::class, 'update']);
-Route::get('/profiles/delete/{id}', [App\Http\Controllers\ProfileController::class, 'delete']);
+Route::get('/profiles/one/{id}', [App\Http\Controllers\ProfileController::class, 'show'])->middleware('auth');
+Route::get('/profiles/edit/{id}', [App\Http\Controllers\ProfileController::class, 'edit'])->middleware('auth');
+Route::put('/profiles/update/{id}', [App\Http\Controllers\ProfileController::class, 'update'])->middleware('auth');
+Route::get('/profiles/delete/{id}', [App\Http\Controllers\ProfileController::class, 'delete'])->middleware('auth');
 
