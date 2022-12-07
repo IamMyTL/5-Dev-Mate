d19bd613c4937b22149907a0d009d47f069e1c52
MyTL <tlambert385@gmail.com>
2022-12-07 11:58:25 +0100

    Ajout des paginations aux interfaces admin

--- resources/views/admin/ads.blade.php
+++ resources/views/admin/ads.blade.php
@@ -23,6 +23,9 @@
                     </a>
                     @endforeach
                 </div>
+                <div class="d-flex justify-content-center mt-5">
+                    {{ $ads->links() }}
+                </div>
             </div>
         </div>
     </div>
--- resources/views/admin/skills.blade.php
+++ resources/views/admin/skills.blade.php
@@ -17,7 +17,7 @@
                     <ul class="list-group">
                         @foreach($skills as $skill)
                             <li class="list-group-item d-flex justify-content-between align-items-center">
-                                {{ $skill->name }} 
+                                <a href="{{ url('/admin/skill/profiles/'.$skill->id) }}" style="text-decoration:none;"> {{$skill->name}} </a>
                                 <div>
                                     <a href="{{ url('/admin/skill/edit/'.$skill->id) }}" class="btn btn-warning active" role="button">Modifier</a>
                                     <a href="{{ url('/admin/skill/delete/'.$skill->id) }}" class="btn btn-danger active" role="button" onclick="return confirm('Etes-vous sûr de vouloir supprimer cette compétence?')">Supprimer</a>
@@ -25,6 +25,9 @@
                             </li>
                         @endforeach
                     </ul>
+                </div>
+                <div class="d-flex justify-content-center mt-5">
+                    {{ $skills->links() }}
                 </div>
             </div>
         </div>
